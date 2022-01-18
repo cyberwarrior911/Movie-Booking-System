e780704d3fc9c635ffa8352e86438be8427a6a84
Mayank Kushwaha <72402072+cyberwarrior911@users.noreply.github.com>
2022-01-19 03:55:32 +0530

    added theater selection

--- movie.py
+++ movie.py
@@ -1,28 +1,34 @@
 #program to make movie ticket thingy
 movies = ('mov1',"mov2",)
-Theaters = ()
+theaters = ("t1","t2","t3",)
+
 location = input("Enter your city : ")
 x = 0
 select = 0 
-def select():
-    global s
+def select_mov():
     s = int(input("\nEnter the serial no of the your selection : "))
     print("You have selected :",movies[s-1])
     conf = input("Press Y to continue, N to reselect ")
     if conf == "Y" or conf == "y" :
         return s
 
-print("\nTrending movies in ",location,"are :")
+print("\nThis week's trending movies are :")
 for i in movies:
     print(x+1,". ",i)
     x=x+1
-    
+x = 0
 
 while True :
-    temp = select()
+    temp = select_mov()
     if temp != None :
         selection =  temp - 1
         break
     
-print(movies[selection])
+#print(movies[selection])
+
+def select_the():
     
+    
+for i in  theaters:
+    print(x+1,". ",i)
+    x += 1
