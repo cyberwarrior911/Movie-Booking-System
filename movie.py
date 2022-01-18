bb4e99269d1d97dceb882e5d0cf5b17667679498
Mayank Kushwaha <72402072+cyberwarrior911@users.noreply.github.com>
2022-01-19 04:24:36 +0530

    theater selection added v.2

--- movie.py
+++ movie.py
@@ -11,6 +11,14 @@ def select_mov():
     conf = input("Press Y to continue, N to reselect ")
     if conf == "Y" or conf == "y" :
         return s
+def select_the():
+    s = int(input("\nEnter the serial no of the your selection : "))
+    print("You have selected ",theaters[s-1],"theater")
+    conf = input("Press Y to continue, N to reselect ")
+    if conf == "Y" or conf == "y" :
+        return s
+        
+#selects movie        
 
 print("\nThis week's trending movies are :")
 for i in movies:
@@ -21,14 +29,21 @@ x = 0
 while True :
     temp = select_mov()
     if temp != None :
-        selection =  temp - 1
+        selection_mov =  temp - 1
         break
     
-#print(movies[selection])
+#selects theater
 
-def select_the():
-    
-    
+print("\nSelect your preferred theater :")
 for i in  theaters:
     print(x+1,". ",i)
     x += 1
+    
+while True :
+    temp = select_the()
+    if temp != None :
+        selection_the =  temp - 1
+        break
+
+print(movies[selection_mov])
+print(theaters[selection_the])
